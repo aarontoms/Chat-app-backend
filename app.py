3d1ab7592b79fb14e8e5ee24f1c7ca1ccf7104cb
aarontoms <aarontoms29@gmail.com>
2025-01-18 18:13:40 +0530

    hosting

--- app.py
+++ app.py
@@ -5,22 +5,18 @@ import os
 
 app = Flask(__name__)
 
-#redis connection
+load_dotenv()
 host = os.getenv("REDIS_HOST")
 port = os.getenv("REDIS_PORT")
 password = os.getenv("REDIS_PASSWORD")
 r = redis.Redis(host=host, port=port, password=password)
 
-groupid = "12345"
-ttl_seconds = 10  # TTL in seconds (10 minutes)
-messageid = r.incr(f"{groupid}:counter")
-r.expire(f"{groupid}:counter", ttl_seconds+10)
-senderid = "user2"
-message_content = "Hello, 1!"
+ttl = 600  
 
-app.route('/send', methods=['POST'])
-def receive():
+app.route('/<groupid>/send', methods=['POST'])
+def receive(groupid):
     data = request.get_json()
+    local_groupid = data.get("local_groupid")
     messageid = data.get("messageid")
     senderid = data.get("sender")
     message_content = data.get("message")
@@ -30,6 +26,6 @@ def receive():
         "message": message_content,
     }
     r.hset(f"{groupid}:messages:{messageid}", mapping=msg)
-    r.expire(f"{groupid}:messages:{messageid}", ttl_seconds)
+    r.expire(f"{groupid}:messages:{messageid}", ttl)
 
     return jsonify({"status": "success"}), 200
