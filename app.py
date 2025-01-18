0712dedfc2078590e958495aa90a9d3e487b0733
aarontoms <aarontoms29@gmail.com>
2025-01-18 18:40:31 +0530

    test

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ r = redis.Redis(host=host, port=port, password=password)
 ttl = 600  
 
 app.route('/<groupid>/send', methods=['POST'])
-def receive(groupid):
+def send(groupid):
     data = request.get_json()
     local_groupid = data.get("local_groupid")
     messageid = data.get("messageid")
@@ -28,4 +28,8 @@ def receive(groupid):
     r.hset(f"{groupid}:messages:{messageid}", mapping=msg)
     r.expire(f"{groupid}:messages:{messageid}", ttl)
 
+    return jsonify({"status": "success"}), 200
+
+app.route('/<groupid>/send', methods=['GET'])
+def test():
     return jsonify({"status": "success"}), 200
